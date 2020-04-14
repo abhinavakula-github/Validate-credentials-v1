ef7346999709c47865e7d6a28daa0b7faa8e57dd
Abhinav Akula VS home <abhinav.akula@accenture.com>
2020-04-14 11:48:51 +0530

    Add project files.

---
+++ Validate credentials/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Validate_credentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userid, pwd;
+            string tempuser, temppwd;
+            string loweruser;
+            int count = 0;
+            tempuser = "Abhinav";
+            temppwd = "HelloWorld";
+            Console.WriteLine("Enter your credentials");
+            Console.WriteLine("userid");
+            userid = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("password");
+            pwd = Convert.ToString(Console.ReadLine());
+            loweruser = userid.ToLower();
+            while (true)
+            {
+                if (count <= 3)
+                {
+                    if ((tempuser.Equals(loweruser) && pwd == temppwd))
+                        Console.WriteLine("you're logged in");
+                    else
+                        count++;
+                }
+                else
+                {
+                    Console.WriteLine("attempts are done");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
